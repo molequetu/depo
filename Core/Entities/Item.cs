5878177d1c4e074870e9e8a9d67959af26a787b4
Ibraim Sapountzi <ibraimsap@gmail.com>
2018-12-05 21:29:49 +0200

    add core .net library project that contains domain and application layers, add first item entity;

---
+++ Core/Entities/Item.cs
@@ -0,0 +1,14 @@
+﻿namespace Depo.Core.Entities
+{
+    /// <summary>
+    /// Represents an item in the
+    /// warehouse. Can be anything,
+    /// its id is a barcode unique id
+    /// </summary>
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
